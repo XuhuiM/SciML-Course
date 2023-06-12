e795ce4e8abe1040ddf419cc4f6ac970ed536aad
Xuhui Meng <47721030+XuhuiM@users.noreply.github.com>
2023-06-12 21:08:19 +0800

    update data for II&III

--- Course-Project/Data/Project-II&III/Project_III.m
+++ Course-Project/Data/Project-II&III/Project_III.m
@@ -27,7 +27,7 @@ figure;
 
 % kr = exp((x - 0.1).^2).*cos(3*x).^2;
 
-lambda = 0.2 + exp(x).*cos(2*x).^2;
+lambda = 0.2 + exp(x.^2).*cos(2*x).^2;
 
 plot(x, lambda, 'linewidth', 2.0);
 
@@ -53,7 +53,7 @@ function [c, f, s] = cdeeq(x, t, u, dudx)
 c = 1;
 f = 0.01*dudx;
 %     kr = -exp((x - 0.1).^2).*cos(3*x).^2;
-lambda = 0.2 + exp(x).*cos(2*x).^2;
+lambda = 0.2 + exp(x.^2).*cos(2*x).^2;
 %     kr = 0.0;
 l = 0.4;
 s = -lambda.*u.^3 + (exp(-0.5*(x - 0.25).^2/l^2)).*sin(3*x).^2;
@@ -61,14 +61,14 @@ end
 
 %%%---Boundary conditions: u_l = u_r = 1---------------------------
 function [pl, ql, pr, qr] = cdebc(xl, ul, xr, ur, t)
-pl = ul - 0.5;
+pl = ul - 1;
 ql = 0;
-pr = ur - 0.5;
+pr = ur - 1;
 qr = 0;
 end
 
 %%%--Initiational conditions: u(x, 0) = cos(pi*x)^2-------------------------
 function u0 = cdeic(x)
 % u0 = cos(pi*x).*cos(pi*x);
-u0 = 0.5*cos(pi*x).^2;
+u0 = cos(pi*x).^2;
 end
